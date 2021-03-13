7e5c19272efe1edcefe86eee294771c97ec7e2a2
Gina <ginagigo1220.dif06@nctu.edu.tw>
2021-03-13 21:31:58 +0800

    new file:   Regularized_linear_regression.py 	new file:   result/1.LSE.jpg 	new file:   result/1.Newton.jpg 	new file:   result/2.LSE.jpg 	new file:   result/2.Newton.jpg 	new file:   result/3.LSE.jpg 	new file:   result/3.Newton.jpg 	new file:   testLU.txt 	new file:   testfile.txt

---
+++ Matrix.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  9 20:41:43 2021
+
+@author: GINATSAI
+"""
+
+class Matrix:
+    # Constructor
+    def __init__(self, dims, fill):
+        self.row = dims[0]
+        self.col = dims[1]
+        # constant
+        if isinstance (fill, (int, float)):
+            self.A = [ [fill] * self.col for i in range(self.row)]
+        # 2d array
+        elif isinstance (fill, list):
+            self.A = fill
+    
+    # print Matrix
+    def __str__(self):
+        mtxStr = ''
+        for r in range(self.row):
+            for c in range(self.col):
+                mtxStr += str(self.A[r][c])
+                if c != self.col - 1:
+                    mtxStr += ', '
+            mtxStr += '\n'
+        return mtxStr
+        
+    # A + B or A + 2
+    def __add__(self, other):
+        # new Matrix
+        C = Matrix(dims = (self.row, self.col), fill = 0)
+        
+        if isinstance (other, Matrix):
+            for i in range(self.row):
+                for j in range(self.col):
+                    C.A[i][j] = self.A[i][j] + other.A[i][j]
+        
+        elif isinstance (other, (int, float)):
+            for i in range(self.row):
+                for j in range(self.col):
+                    C.A[i][j] += other
+        
+        return C
+    
+    # right-hand-side 
+    def __radd__(self, other):
+        return self.__add__(other)
+    
+    def __sub__(self, other):
+        # new Matrix
+        C = Matrix(dims = (self.row, self.col), fill = 0)
+        
+        if isinstance (other, Matrix):
+            for i in range(self.row):
+                for j in range(self.col):
+                    C.A[i][j] = self.A[i][j] - other.A[i][j]
+        
+        elif isinstance (other, (int, float)):
+            for i in range(self.row):
+                for j in range(self.col):
+                    C.A[i][j] -= other
+        
+        return C
+    
+    # A * 2 or A *b matrix-matrix pointwise multiplication
+    def __mul__(self, other):
+        C = Matrix(dims = (self.row, self.col), fill = 1)
+        
+        # Scalar multiplication
+        if isinstance(other, (int, float)):
+            for i in range(self.row):
+                for j in range(self.col):
+                    C.A[i][j] = self.A[i][j] * other
+        elif isinstance(other, Matrix):
+            for i in range(self.row):
+                for j in range(self.col):
+                    C.A[i][j] = self.A[i][j] * other.A[i][j]
+        return C
+                    
+        
+    # 2.0 * A
+    def __rmul__(self, other):
+        return self.__mul__(other)
+    
+    # A @ B matrix-matrix multiplication
+    def __matmul__(self, other):
+        if isinstance (other, Matrix):
+            C = Matrix(dims = (self.row, other.col), fill = 0)
+            if self.col != other.row:
+                print("InValid multiplication")
+                return
+            
+            for i in range(self.row):
+                for j in range(other.col):
+                    tmp = 0
+                    for k in range(self.col):
+                        tmp = tmp + self.A[i][k] * other.A[k][j]
+                    C.A[i][j] = tmp
+        return C
+    
+    # Matrix Transpose
+    def T(self):
+        C = Matrix(dims = (self.col, self.row), fill = 0)
+        for i in range(self.row):
+            for j in range(self.col):
+                C.A[j][i] = self.A[i][j]
+        return C
+    
+    def __getitem__(self, key):
+        if isinstance(key, tuple):
+            i, j = key[0], key[1]
+            return self.A[i][j]
+    
+    def __setitem__(self, key, value):
+        if isinstance(key, tuple):
+            i, j = key[0], key[1]
+            self.A[i][j] = value
+            
+                    
+
+if __name__ == '__main__':
+    # test
+    A = Matrix(dims = (3,2), fill = 1)
+    
+    B = Matrix(dims = (2,6), fill = 6.6)
+    C = A @ B
+    print(C)
+    tmp = C.T()
+    print(tmp)
+    tmp = tmp @ C
+    print(C.T() @ C)
+    D = Matrix(dims = (3, 2), fill = [[1, 2], [3, 4], [5, 6]])
+    print(D)
+    G = Matrix(dims = (2, 2), fill = 2)
+    print(D @ G)
+    A[0,0]
+    A[0,0] = 2
+    print(A)
---
+++ Regularized_linear_regression.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 22:16:37 2021
+
+@author: GINATSAI
+content: 
+"""
+from Matrix import Matrix
+import matplotlib.pyplot as plt
+import time # for calculating the execution time
+
+def IdentityMatrix(n):
+    I = Matrix(dims = (n, n), fill = 0)
+    for i in range(n):
+        I[i, i] = 1
+    return I
+
+def findLU(n, matrixA):
+    # Find the L U for the decomposition
+    L = Matrix(dims = (n, n), fill = 0)
+    U = Matrix(dims = (n, n), fill = 0)
+    
+    # Fill the value of L
+    for i in range(n):
+        L[i, i] = 1
+    
+    # Calculate for the lower, upper matrix, i for row index, j for col index
+    for i in range(n):
+        for j in range(n):
+            tmp = matrixA[i, j] 
+            for k in range(n):
+                if j != k:
+                    tmp = tmp -  L[i, k] * U[k, j]
+
+            # lower matrix     
+            if i > j :
+                if U[j , j] != 0:
+                    L[i, j] = tmp / U[j, j]
+                else :
+                    L[i, j] = tmp
+            # Upper matrix
+            else:
+                if L[j, j] != 0:
+                    U[i, j] = tmp / L[j, j]  
+                else:
+                    U[i, j] = tmp
+    return L, U
+
+def inverse(n, matrixA):
+    # Ax = I => LUx = I
+    # Find the L U for the decomposition
+    L, U = findLU(n, matrixA)
+                    
+    # LZ = c = I -> c using forward substitution
+    C = IdentityMatrix(n)
+    Z = Matrix(dims = (n, n), fill = 0)
+    for i in range(n):
+        for j in range(n):
+            tmp = C[i, j]
+            #text = str(i) + ', ' + str(j) + ' : ' + str(C[i, j])
+            for k in range(n):
+                if k!= i:
+                    tmp -= L[i, k] * Z[k, j]
+                    #text += ' - ' + str(L[i, k] * Z[k, j])
+            if L[i, i] != 0:
+                Z[i, j] = tmp / L[i, i]
+            else:
+                Z[i, j] = tmp
+            #text += ' = ' + str(tmp) + ' / ' + str(L[i, i])
+            #print(text)
+            
+    #print(L @ Z) # should be indentity matrix
+    #print(Z)
+    
+    # Ux = Z using backward substitution
+    X = Matrix(dims = (n, n), fill = 0)
+    for i in range(n-1, -1, -1):
+        for j in range(n):
+            tmp = Z[i, j]
+            #text = str(i) + ', ' + str(j) + ' : ' + str(Z[i, j])
+            for k in range(n):
+                if k!= i:
+                    tmp -= U[i, k] * X[k, j]
+                    #text += ' - ' + str(U[i, k] * X[k, j])
+            if U[i, i] != 0:
+                X[i, j] = tmp / U[i, i]
+            else:
+                X[i, j] = tmp
+            #text += ' = ' + str(tmp) + ' / ' + str(U[i, i]) + ' = ' + str(X[i, j])
+            #print(text)
+            
+    #print(U @ X) # should be Z
+    #print(Z)
+    #print(result @ X) # near the identity matrix
+    #print(X)
+    
+    return X
+    
+def fillMatrix(x, y, base_n):
+    # create computing matrix
+    matrixA = []
+    matrixb = []
+    for data in x:
+        tmp = []
+        for j in range(base_n - 1, -1, -1):
+            tmp.append(pow(data, j))
+        matrixA.append(tmp)
+    for data in y:
+        matrixb.append([data])
+    
+    A = Matrix(dims = (len(x), base_n), fill = matrixA)
+    b = Matrix(dims = (len(y), 1), fill = matrixb)
+    return A, b
+                    
+def LSE(A, b, lamd):
+    # Identity matrix
+    I = IdentityMatrix(n)
+    
+    # A.Tranpose @ A + lamda(scalar) * I
+    x = A.T() @ A + lamd * I
+    x_inv = inverse(x.row, x);
+    x = x_inv @ A.T() @ b
+    return x
+
+def Newton(A, b, n):
+    # initialize the x0 to whatever
+    x0 = []
+    for i in range(n):
+        x0.append([0])
+    x0 = Matrix(dims = (n, 1), fill = x0)
+    
+    value = 1
+    times = 0
+    while value > 0.001:
+    
+        deltaF = 2 * A.T() @ A @ x0 - 2 * A.T() @ b
+        HessionF = 2 * A.T() @ A
+        
+        x1 = x0 - inverse(HessionF.row, HessionF) @ deltaF
+        
+        # calculate x[i] - x[i-1] value
+        tmp = x1 - x0
+        value = 0
+        for i in range(n):
+            value += tmp[i, 0]
+        x0 = x1
+        times += 1
+    
+    print("Newton's method 迭代次數:", times - 1)
+    return x1
+
+def printLine(x):
+    output = ""
+    for i in range(x.row - 1):
+        output = output + str(x[i, 0]) + "x^" + str(x.row - 1 - i) + " + "
+    output = output + str(x[ x.row - 1, 0])
+    print("Fitting ine : ", output)
+
+def computeError(A, X, b):
+    predictY = A @ X
+    error = predictY - b
+    
+    errorSum = 0
+    for i in range(b.row):
+        errorSum += error[i, 0] * error[i, 0]
+    print("Total error : ", errorSum)
+    
+def plotline(x, y, line, n):
+    predictY = []
+    # i for number of y
+    for i in range(len(x)):
+        tmp = 0
+        # j for power
+        for j in range(n):
+            tmp += line[j, 0] * x[i] ** (n - 1 - j)
+        predictY.append(tmp)
+    #print(predictY)
+    
+    plt.grid()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.axvline(color = 'k') # black
+    plt.axhline(color = 'k')
+    
+    plt.scatter(x, y, color = 'r', edgecolors = 'k') # red
+    plt.plot(x, predictY, color = 'b', linewidth = 2.0) # blue    
+    plt.show()
+    
+# read file
+f = open('testfile.txt', 'r')
+lines = f.readlines()
+x = []
+y = []
+for line in lines:
+    text = line.split(',')
+    x.append(float(text[0]))
+    y.append(float(text[1]))
+
+# LSE
+n = int(input("plz enter the number of base : "))
+lamb = float(input("plz enter the lambda : "))
+A, b = fillMatrix(x, y, n);
+
+print("\n---------- LSE -----------")
+start_time = time.clock()
+line1 = LSE(A, b, lamb)
+print("LSE takes ",time.clock() - start_time, "s")
+printLine(line1)
+computeError(A, line1, b)
+plotline(x, y, line1, n)
+
+# Newton's method in optimization
+print("---------- Newton's method -----------")
+start_time = time.clock()
+line2 = Newton(A, b, n)
+print("Newton's method takes", time.clock() - start_time, "s")
+computeError(A, line2, b)
+plotline(x, y, line2, n)
